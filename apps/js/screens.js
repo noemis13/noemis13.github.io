45d93896fe0e40a5da303d8e01deb5cf701b57c0
noemis13 <noemischerer13@gmail.com>
2017-11-21 15:25:02 -0200

    mano, tava funcionando

--- apps/js/screens.js
+++ apps/js/screens.js
@@ -33,18 +33,13 @@ class PlayState extends GameState {
 
         gyro.frequency = 10;
         
-        gyro.startTracking(function(o) {
-            // updating player velocity
-            this.player1.body.velocity.x += o.gamma/20;
-            this.player1.body.velocity.y += o.beta/20;
-       });
         
         // Biblioteca Gyro
-        /*if(Phaser.Device.iOS) {	
+        if(Phaser.Device.iOS) {	
             this.player1.body.velocity.x = -(document.body.getAttribute("beta") * 10);
         } else {	
             this.player1.body.velocity.x = document.body.getAttribute("beta") * 10;
-        }*/
+        }
 
         // mapa com paredes
        // this.createMap()
