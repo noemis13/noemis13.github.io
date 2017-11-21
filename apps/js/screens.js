1c33b1caacdc0ccacadb43fc77d425ece0b24800
noemis13 <noemischerer13@gmail.com>
2017-11-21 19:49:25 -0200

    mudando tudo

--- apps/js/screens.js
+++ apps/js/screens.js
@@ -69,8 +69,8 @@ class PlayState extends GameState {
         var z = e.alpha;
         var y = e.beta;
         var x = e.gamma;
-        this.player1.body.velocity.x += x/20;
-        this.player1.body.velocity.y += y/20;
+        this.player1.body.velocity.x -= x*2;
+        this.player1.body.velocity.y -= y*2;
     }
 
     updateHud() {
