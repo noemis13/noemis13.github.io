bab38d99636201f0f34b0384a2dde5586d884168
noemis13 <noemischerer13@gmail.com>
2017-11-21 16:37:58 -0200

    testentado  gyro

--- apps/js/screens.js
+++ apps/js/screens.js
@@ -36,9 +36,9 @@ class PlayState extends GameState {
         
         // Biblioteca Gyro
         if(Phaser.Device.iOS) {	
-            this.player.body.velocity.x = -(document.body.getAttribute("beta") * 60);
+            this.player1.body.velocity.x = -(document.body.getAttribute("beta") * 60);
         } else {	
-            this.player.body.velocity.x = document.body.getAttribute("beta") * 60;
+            this.player1.body.velocity.x = document.body.getAttribute("beta") * 60;
         }
         // mapa com paredes
        // this.createMap()
