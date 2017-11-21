16bfaa200e769e8249502f47f414f3bd5d3899a4
noemis13 <noemischerer13@gmail.com>
2017-11-20 22:36:21 -0200

    gyroo

--- apps/js/screens.js
+++ apps/js/screens.js
@@ -33,7 +33,12 @@ class PlayState extends GameState {
 
         // Biblioteca Gyro
         gyro.frequency = 10;
-       		
+           
+        gyro.startTracking(function(o){
+            this.player1.body.velocity.x = o.gamma/20
+            this.player1.body.velocity.y = o.beta/20 
+
+        })
  
         // mapa com paredes
        // this.createMap()
