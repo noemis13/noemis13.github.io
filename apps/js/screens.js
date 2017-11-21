dc094c962dff160eed90da08b02a29bb40de457f
noemis13 <noemischerer13@gmail.com>
2017-11-21 20:34:02 -0200

    obstacle

--- apps/js/screens.js
+++ apps/js/screens.js
@@ -4,7 +4,7 @@ class PlayState extends GameState {
     preload() {
         this.game.load.image('player', 'assets/ball.png')
         this.game.load.image('background', 'assets/fundoMadeira.jpg')
-        this.game.load.image('shot', 'assets/shot.png')
+        this.game.load.image('box', 'assets/box.png')
         this.game.load.image('saw', 'assets/saw.png')
         this.game.load.spritesheet('explosion', 'assets/explosion.png', 56, 56)
         this.game.load.image('fullscreen-button', 'assets/fullscreen-button.png')
@@ -21,8 +21,12 @@ class PlayState extends GameState {
        //let background = this.game.add.tileSprite(0, 0, this.game.width, this.game.height, 'background')
       //  background.autoScroll(-30, 0)
 
-        // players
+
+        // Mapa do jogo
+        //this.createMap()
+        this.createMap_old()
         
+        // players
         this.player1 = this.game.add.sprite(this.game.width/1.5, this.game.height-140, 'player')
         this.player1.anchor.setTo(0.5, 0.5)
         this.player1.scale.setTo(0.02, 0.02)
@@ -40,6 +44,59 @@ class PlayState extends GameState {
         super.initFullScreenButtons()
     }
 
+    
+
+    /*createMap(){
+        let mapTmx = this.game.add.tilemap('level1');
+        this.game.world.setBounds(0, 0, mapTmx.widthInPixels, mapTmx.heightInPixels);
+
+        this.map = this.game.add.group()   
+        mapTmx.createFromObjects('Object Layer 1', 1, 'box', 0, true, false, this.map, Block);
+
+    }*/
+
+    createMap_old() {
+        let mapData = [ "  X                     X                    ",
+                        "                                             ",
+                        "                                             ",
+                        "                                             ",
+                        "             X                     X         ",
+                        "                                             ",
+                        "                                             ",
+                        "                                             ",
+                        "  X                     X                    ",
+                        "                                             ",
+                        "                                             ",
+                        "                                             "]
+                        
+        this.map = this.game.add.group()
+        for (let row = 0; row < mapData.length; row++) {
+            for (let col = 0; col < mapData[0].length; col++) {
+                if (mapData[row][col] == 'X') {
+                    let block = this.map.create(col*32, row*32, 'box')
+                    //block.width = 60
+                    //block.height= 60
+                    //this.map.add(block)
+                    this.game.physics.arcade.enable(block)
+                    block.body.immovable = true
+                    block.tag = 'box'
+                    block.autoCull = true
+                    block.scale.setTo(0.2, 0.2)
+                    block.inputEnabled = true
+                    block.input.enableDrag(false, true)        
+                }
+            }
+        }
+    }
+
+
+    handleOrientation(e) {
+        var z = e.alpha;
+        var y = e.beta;
+        var x = e.gamma;
+        this.player1.body.velocity.x -= x*2;
+        this.player1.body.velocity.y -= y*4;
+    }
 
     createHealthText(x, y, string) {
         let style = {font: 'bold 16px Arial', fill: 'white'}
@@ -50,15 +107,15 @@ class PlayState extends GameState {
     }
 
     update() { 
-        // Mover player
+        // Colisão
+        this.physics.arcade.collide(this.player1, this.map, this.boxCollision, null, this);
+        
     }
 
-    handleOrientation(e) {
-        var z = e.alpha;
-        var y = e.beta;
-        var x = e.gamma;
-        this.player1.body.velocity.x += y*2;
-        this.player1.body.velocity.y += x*4;
+    boxCollision() {
+        if("vibrate" in window.navigator) {
+            window.navigator.vibrate(100);
+        }
     }
 
     updateHud() {
