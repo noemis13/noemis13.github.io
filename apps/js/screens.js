bef4104d526e502aa7268e7d9088e39247a5b184
noemis13 <noemischerer13@gmail.com>
2017-11-28 22:05:04 -0200

    .

--- apps/js/screens.js
+++ apps/js/screens.js
@@ -98,8 +98,8 @@ class PlayState extends GameState {
         var z = e.alpha;
         var y = e.beta;
         var x = e.gamma;
-        this.player1.body.velocity.x += x*2;
-        this.player1.body.velocity.y += y*4;
+        this.player1.body.velocity.x += x;
+        this.player1.body.velocity.y += y;
     }
 
     createHealthText(x, y, string) {
