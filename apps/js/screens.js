a96195c32598ece9f43e03d1989fafc976e8b8fb
noemis13 <noemischerer13@gmail.com>
2017-12-11 19:47:59 -0200

    arrumando o level

--- apps/js/screens.js
+++ apps/js/screens.js
@@ -56,7 +56,7 @@ class PlayState extends GameState {
 
     createHud(){
         // HUD
-        this.textVersion = this.createHealthText(this.game.width*5/9, 40, 'N9')
+        this.textVersion = this.createHealthText(this.game.width*5/9, 40, 'N1')
         this.textVersion.fixedToCamera = true
         
        // Pontuação
