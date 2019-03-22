b55ac2c9de7016866977e14bb54150f13c4af48e
noemis13 <noemischerer13@gmail.com>
2019-03-22 22:33:02 +0000

    fixing

--- apps/js/screens.js
+++ apps/js/screens.js
@@ -418,7 +418,7 @@ class PlayState extends GameState {
         }
 
         
-        alert('Too bad, you\'ve died! :(');
+        alert('Too bad, you have died! :(');
         
         this.map.destroy()
         this.holeMap.destroy()
